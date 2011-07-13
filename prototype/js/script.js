f60ef407770d0f4d5d292c0f7d9c2d70880d99f7
Philipp Gildein <rmbl@pgserv.net>
2011-07-13 14:32:52 +0200

    Fix settings icon dropdown

--- prototype/js/script.js
+++ prototype/js/script.js
@@ -5,7 +5,7 @@
 $(function() {
     $('header .settings-icon a').click(function() {
         var elem = $(this).parent();
-        elem.toggleClass('settings-icon-overlay');
+        elem.toggleClass('settings-icon-overlay settings-icon-no-overlay');
         elem.next().toggle();
     });
 });
