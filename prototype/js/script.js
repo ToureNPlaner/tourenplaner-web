5d9087ee3593b80fae9fe0dee83a369fc5d3d57c
Philipp Gildein <rmbl@pgserv.net>
2011-07-13 14:58:17 +0200

    Add jquery-ui and make the sidebar resizable

--- prototype/js/script.js
+++ prototype/js/script.js
@@ -3,9 +3,15 @@
 */
 
 $(function() {
+    // Header settings dropdown
     $('header .settings-icon a').click(function() {
         var elem = $(this).parent();
         elem.toggleClass('settings-icon-overlay settings-icon-no-overlay');
-        elem.next().toggle();
+        elem.next().toggle('blind');
+    });
+    
+    // Resizable result area
+    $('#main #sidebar').resizable({
+        handles: 'e'
     });
 });
