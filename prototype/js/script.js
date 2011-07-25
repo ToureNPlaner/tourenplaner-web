39950c96b4dda73bedb987bfa3946ef65d172dd8
Philipp Gildein <rmbl@openspeak-project.org>
2011-07-25 20:44:39 +0200

    Fix background-color for some elements

--- prototype/js/script.js
+++ prototype/js/script.js
@@ -13,33 +13,33 @@ $(function() {
         elem.toggleClass('settings-icon-overlay settings-icon-no-overlay');
         elem.next().toggle('blind');
     });
-    
+
     // Resizable result area
     $('#main #sidebar').resizable({
         handles: 'e'
     });
-    
+
     // update map size when resize sidebar
     var resizeUpdater = function(event, ui) {
     	$("#main #map").css('left', ui.size.width);
     	$("#main #map").css('width', "100%");
     	$("#main #map").css('height', "100%");
     	mapObject.refresh();
-    }
+    };
     $("#main #sidebar").bind("resize", resizeUpdater);
     $("#main #sidebar").bind("resizestop", resizeUpdater);
-    
+
     $('#main #data').resizable({
-       handles: 'w,nw,n' 
+       handles: 'w,nw,n'
     });
-    
+
     $('#main #data #minmax a').click(function() {
         $('#main #data #content').toggle();
         var parent = $('#main #data');
         parent.toggleClass('minimized');
         if ($(this).html() == '_') {
             $(this).html('Daten');
-            
+
             data_style = parent.attr('style');
             parent.attr('style', '');
         } else {
@@ -47,7 +47,7 @@ $(function() {
             parent.attr('style', data_style);
         }
     });
-    
+
     $('header .login a').click(function() {
         $('#login').dialog({
             modal: true,
@@ -57,7 +57,7 @@ $(function() {
                     var valid = true;
                     var username = $('#login form #username').val();
                     var password = $('#login form #password').val();
-                    
+
                     if (username.length == 0) {
                         valid = false;
                         $('#login form #username').addClass('ui-state-error')
@@ -66,7 +66,7 @@ $(function() {
                         valid = false;
                         $('#login form #password').addClass('ui-state-error');
                     }
-                    
+
                     if (valid) {
                         user = { username: username, password: password };
                         log(user);
@@ -74,10 +74,10 @@ $(function() {
                     } else {
                         $('.validate', this).show();
                     }
-                },                
+                },
                 "Cancel" : function() {
                     $(this).dialog('close');
-                }            
+                }
             },
             close: function() {
                 $('input', this).val('').removeClass('ui-state-error');
