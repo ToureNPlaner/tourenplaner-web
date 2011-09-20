a6ba5222dfda50a43676640827b81afbeade37e0
Philipp Gildein <rmbl@openspeak-project.org>
2011-09-20 18:35:19 +0200

    Add /info to mock api and fix the login tests using the new api object

--- src/test/tests.js
+++ src/test/tests.js
@@ -2,26 +2,61 @@
 // documentation on writing tests here: http://docs.jquery.com/QUnit
 // example tests: https://github.com/jquery/qunit/blob/master/test/same.js
 
-// below are some general tests but feel free to delete them.
+// these tests things from plugins.js
+module("plugins.js");
+test("Log function/Modernizr", function(){
+    ok(!!window.log, "log function present");
 
-window.testApi = new Api();
+    var history = log.history && log.history.length || 0;
+    log("logging from the test suite.")
+    equals(log.history.length - history, 1, "log history keeps track")
 
-// these test things from plugins.js
-test("Log function/Modernizr",function(){
-  expect(3);
-  ok( !!window.log, "log function present");
-
-  var history = log.history && log.history.length || 0;
-  log("logging from the test suite.")
-  equals( log.history.length - history, 1, "log history keeps track" )
-
-  ok( !!window.Modernizr, "Modernizr global is present")
+    ok(!!window.Modernizr, "Modernizr global is present")
 });
 
-test("Login function", function() {
-  expect(3);
-  
-  ok( !! testApi.authUser('asd', 'asd'), "login working for asd:asd");
-  ok( ! testApi.authUser('bsd', 'asd'), "login not working for bsd:asd");
-  ok( ! testApi.authUser('asd', 'bsd'), "login not working for asd:bsd");
+api = new Api();
+api.set({
+    server: null,
+    authRequired: true
+});
+
+module("api.js");
+test("/info", function() {
+    var ret = api.serverInformation();
+    //TODO: Nothing to test yet
+});
+
+test("/authuser", 5, function() {
+    ok(!api.authUser(), 'Empy function call');
+    ok(!api.authUser({}), 'Empty object');
+
+    stop();
+    api.authUser({
+        email: 'asd@asd.de',
+        password: 'asd',
+        callback: function(text, success) {
+            same(success, true, 'Login working for asd@asd.de:asd');
+        }
+    });
+
+
+    api.authUser({
+        email: 'bsd@asd.de',
+        password: 'asd',
+        callback: function(text, success) {
+            same(success, false, "Login not working for bsd@asd.de:asd");
+        }
+    });
+
+    api.authUser({
+        email: 'asd@asd.de',
+        password: 'bsd',
+        callback: function(text, success) {
+            same(success, false, 'Login not working for asd@asd.de:bsd');
+        }
+    });
+
+    setTimeout(function() {
+        start();
+    }, 1000);
 });
