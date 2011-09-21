df11693b23dc7f0ac876c51048fb5efed0d20e77
Philipp Gildein <rmbl@pgserv.net>
2011-09-21 15:34:28 +0200

    Login now uses the api and the site as a whole uses the mockapi for easier testing

--- src/js/mylibs/views.js
+++ src/js/mylibs/views.js
@@ -25,8 +25,9 @@ window.TopbarView = Backbone.View.extend({
 
     initialize: function() {
         window.app.user.bind('login', this.onLogin, this);
-
-        this.$('ul li.menu, ul li.user').hide();
+        this.$('ul li.menu, ul li.user').each(function() {
+            $(this).css('display', 'block').hide();
+        });
     },
 
     hideDropdown: function() {
@@ -40,10 +41,15 @@ window.TopbarView = Backbone.View.extend({
 
     onLogin: function(login) {
         var user = window.app.user;
-        this.$('ul li').toggle();
 
-        if (user.isLoggedIn())
+        if (user.isLoggedIn()) {
             this.$('li.user a').html(user.get('firstname') + ' ' + user.get('lastname'));
+            this.$('li.user, li.menu').show();
+            this.$('li.login-link, li.register-link').hide();
+        } else {
+            this.$('li.user, li.menu').hide();
+            this.$('li.login-link, li.register-link').show();
+        }
     }
 
 });
@@ -87,8 +93,6 @@ mapObject.refresh();
 // $("#main #sidebar").bind("resizestop", resizeUpdater);
 $("#main #sidebar").bind("resize", resizeUpdater);
 
-
-
 var mapObject;
 window.MapView = Backbone.View.extend({
 
@@ -97,13 +101,13 @@ window.MapView = Backbone.View.extend({
     initialize: function(args) {
         this.onResize(args.sidebar);
         $(window).resize(_.bind(this.onResize, this));
-	mapObject = new Map("map");
-	setContextMenu();
-	mapObject.refresh();
-	
-	/* FOR TESTING */
-	mapObject.drawRoute("[9.219390,48.680170,0.000000],[9.219080,48.680060,0.000000],[9.219080,48.680060,0.000000],[9.219190,48.679820,0.000000],[9.219700,48.679140,0.000000],[9.219810,48.678840,0.000000],[9.219810,48.678700,0.000000],[9.219810,48.678700,0.000000],[9.218010,48.678460,0.000000],[9.216390,48.678180,0.000000],[9.216210,48.678110,0.000000],[9.216210,48.678110,0.000000],[9.216430,48.677350,0.000000],[9.217050,48.676210,0.000000],[9.217050,48.676210,0.000000],[9.216960,48.676060,0.000000],[9.216670,48.675870,0.000000],[9.216390,48.674940,0.000000],[9.216180,48.674640,0.000000],[9.215950,48.674430,0.000000],[9.215380,48.674090,0.000000],[9.214870,48.673720,0.000000],[9.214500,48.673290,0.000000],[9.214130,48.672400,0.000000],[9.213890,48.671620,0.000000],[9.213600,48.671060,0.000000],[9.213360,48.670800,0.000000],[9.212360,48.669990,0.000000],[9.212360,48.669990,0.000000],[9.212200,48.669980,0.000000],[9.212210,48.669870,0.000000],[9.212280,48.669850,0.000000],[9.212390,48.669890,0.000000],[9.213250,48.669290,0.000000],[9.213250,48.669290,0.000000],[9.212990,48.669190,0.000000],[9.212910,48.669190,0.000000],[9.212370,48.669320,0.000000],[9.212190,48.669320,0.000000],[9.211310,48.669000,0.000000],[9.211160,48.668880,0.000000],[9.211160,48.668880,0.000000],[9.211040,48.668720,0.000000],[9.211180,48.668140,0.000000],[9.211180,48.667870,0.000000]");
-	
+    mapObject = new Map("map");
+    setContextMenu();
+    mapObject.refresh();
+
+    /* FOR TESTING */
+    mapObject.drawRoute("[9.219390,48.680170,0.000000],[9.219080,48.680060,0.000000],[9.219080,48.680060,0.000000],[9.219190,48.679820,0.000000],[9.219700,48.679140,0.000000],[9.219810,48.678840,0.000000],[9.219810,48.678700,0.000000],[9.219810,48.678700,0.000000],[9.218010,48.678460,0.000000],[9.216390,48.678180,0.000000],[9.216210,48.678110,0.000000],[9.216210,48.678110,0.000000],[9.216430,48.677350,0.000000],[9.217050,48.676210,0.000000],[9.217050,48.676210,0.000000],[9.216960,48.676060,0.000000],[9.216670,48.675870,0.000000],[9.216390,48.674940,0.000000],[9.216180,48.674640,0.000000],[9.215950,48.674430,0.000000],[9.215380,48.674090,0.000000],[9.214870,48.673720,0.000000],[9.214500,48.673290,0.000000],[9.214130,48.672400,0.000000],[9.213890,48.671620,0.000000],[9.213600,48.671060,0.000000],[9.213360,48.670800,0.000000],[9.212360,48.669990,0.000000],[9.212360,48.669990,0.000000],[9.212200,48.669980,0.000000],[9.212210,48.669870,0.000000],[9.212280,48.669850,0.000000],[9.212390,48.669890,0.000000],[9.213250,48.669290,0.000000],[9.213250,48.669290,0.000000],[9.212990,48.669190,0.000000],[9.212910,48.669190,0.000000],[9.212370,48.669320,0.000000],[9.212190,48.669320,0.000000],[9.211310,48.669000,0.000000],[9.211160,48.668880,0.000000],[9.211160,48.668880,0.000000],[9.211040,48.668720,0.000000],[9.211180,48.668140,0.000000],[9.211180,48.667870,0.000000]");
+
     },
 
     onResize: function(sidebar) {
@@ -156,11 +160,13 @@ window.DataView = Backbone.View.extend({
 });
 
 window.LoginView = Backbone.View.extend({
-   
+
     el: $('#login'),
-    
-    initialize: function() { },
-   
+
+    initialize: function() {
+        window.app.user.bind('login', _.bind(this.onLoginSuccess, this));
+    },
+
     render: function() {
         this.el.dialog({
             modal: true,
@@ -171,23 +177,31 @@ window.LoginView = Backbone.View.extend({
             },
             close: _.bind(this.onClose, this)
         });
-        
+
         return this;
     },
-    
+
     remove: function() {
         this.el.dialog('close');
     },
-    
+
     onLogin: function() {
-        var username = this.$('input#email').val();
+        var email = this.$('input#email').val();
         var password = this.$('input#password').val();
-        if (!window.app.user.login(username, password))
+        if (!window.app.user.login(email, password))
             this.$('.validate').show();
         else
             this.remove();
     },
-    
+
+    onLoginSuccess: function(success) {
+        if (success) {
+            this.remove();
+        } else {
+            this.$('.validate').show();
+        }
+    },
+
     onClose: function() {
         this.$('input').val('').removeClass('ui-state-error');
         this.$('.validate').hide();
