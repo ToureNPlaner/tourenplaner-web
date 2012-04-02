188eaa309e9ac60fe03e2070652cb2e49f79dd82
Huy Le <lehy@studi.informatik.uni-stuttgart.de>
2012-04-02 17:17:20 +0200

    catch enter key in input fields

--- src/js/mylibs/views/data.js
+++ src/js/mylibs/views/data.js
@@ -60,6 +60,11 @@ window.DataView = Backbone.View.extend({
 			
 			var initValue = this.$('#dataview #pc_' + key).val();
             this.$('#dataview #pc_' + key).spinit({height: 30, initValue: initValue, min: pointconstraints[i].min, max: pointconstraints[i].max});
+            this.$('#pc_' + key).keyup(function (e) {
+                if ((e.keyCode == 13)) {
+                    // avoid submitting the formula when hitting enter
+                }
+            });
 		}
 	
 
